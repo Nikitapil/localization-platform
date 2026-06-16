7a216830711018a7f090eb1ef43c33b5f5b1ac37
Nikitapil <nikitapiliavets@gmail.com>
2026-06-16 23:44:25 +0300

    add getLangsStatistics api method

--- backend/src/modules/statistics/statistics.service.ts
+++ backend/src/modules/statistics/statistics.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserToken } from '../auth/types';
-import { select } from '@inquirer/prompts';
 import { LangsStatisticResponseDto } from './dto/Responses/LangsStatisticResponseDto';
 
 @Injectable()
